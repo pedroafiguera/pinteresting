21cb23453c9a3a06bc9f295d98c907484a4c068b
Pedro Figuera <pedroafiguera@gmail.com>
2014-03-02 19:42:19 -0500

    amazon s3 to heroku

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'jbuilder', '~> 1.2'
 gem 'bootstrap-sass', :git => 'git://github.com/thomas-mcdonald/bootstrap-sass.git'
 gem 'devise', '~> 3.2.3'
 gem 'paperclip', '~> 3.0'
+gem 'aws-sdk', '~> 1.20.0'
 
 group :development, :test do
 	gem 'sqlite3'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,13 @@ Pinteresting::Application.configure do
 
   config.action_mailer.default_url_options = { :host => 'http://onemonthrails-app.herokuapp.com/' }
 
+  #images Paperclick to Amazon S3
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
 end
